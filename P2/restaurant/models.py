539bbd9042c6ebdf5faee8b843ef328730a199d4
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-12 15:18:51 -0500

    Restaurant model updated

--- P2/restaurant/models.py
+++ P2/restaurant/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import SET_NULL
+from users.models import MyUser
 
 # Create your models here.
 
@@ -8,8 +10,15 @@ class Restaurant(models.Model):
     logo = models.ImageField()
     postal_code = models.CharField(max_length=7) 
     phone_number = models.CharField(max_length=20) 
-
+    owner = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True, related_name='owner')
 
 class ImageModel(models.Model):
-    main_image = models.ImageField()
-    image = models.ForeignKey(Restaurant)
+    image = models.ImageField()
+    rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL,  null=True, related_name='restaurant')
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    image = models.ImageField()
+    rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL,  null=True, related_name='restaurant')
