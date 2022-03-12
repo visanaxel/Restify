abbff2beccba4535f54283482a5f4edd54835584
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-12 15:09:37 -0500

    Restaurant model added

--- P2/restaurant/models.py
+++ P2/restaurant/models.py
@@ -4,5 +4,12 @@ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    logo = models.ImageField()
+    postal_code = models.CharField(max_length=7) 
+    phone_number = models.CharField(max_length=20) 
 
-class 
+
+class ImageModel(models.Model):
+    main_image = models.ImageField()
+    image = models.ForeignKey(Restaurant)
