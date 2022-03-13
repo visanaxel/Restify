fd8f63500f18b2b9fff47b2b2d8d6f94703d776a
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-03-12 22:47:58 -0500

    Implemented the views for viewing blog, editing and creating

--- P2/blog/urls.py
+++ P2/blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
+
+from blog.views import EditBlogAPIView, RegisterAPIView, ViewBlogAPIView
 app_name = 'blog'
 
+
 urlpatterns = [
-    
+    path('add/', RegisterAPIView.as_view(), name='add'),
+    path('view/<pk>', ViewBlogAPIView.as_view(), name='view'),
+    path('edit/<pk>', EditBlogAPIView.as_view(), name='edit'),
 ]
