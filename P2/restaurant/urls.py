fc890e2c78e0d0ed821addff800703b217e6196f
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-13 01:40:27 -0500

    Fixed url patterns

--- P2/restaurant/urls.py
+++ P2/restaurant/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('<restaurant_id>/menu/', ViewMenu.as_view(), name='menu'),
     path('<restaurant_id>/menu/add/', AddItem.as_view(), name='add'),
     path('<restaurant_id>/menu/<int:pk>/edit/', EditItem.as_view(), name='edit'),
-] 
+    path('view/<pk>', RestaurantView.as_view(), name='view_rest'),
+    path('add/', AddRestaurantView.as_view(), name='add_restaurant'),
+    path('edit/<pk>', EditRestaurantView.as_view(), name='edit_restaurant')
+]
