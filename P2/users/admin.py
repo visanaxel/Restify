d6894e9aeace385afaf89981d31be3e3b5b2b021
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-03-12 23:02:39 -0500

    added the abillity to view additional fields on admin panel

--- P2/users/admin.py
+++ P2/users/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import MyUser
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,  # original form fieldsets, expanded
+        (                      # new fieldset added on to the bottom
+            'Custom Field Heading',  # group heading of your choice; set to None for a blank space instead of a header
+            {
+                'fields': (
+                    'description',
+                    'profile_pic',
+                    'is_owner'
+                ),
+            },
+        ),
+    )
+
 # Register your models here.
-admin.site.register(MyUser, UserAdmin)
+admin.site.register(MyUser, CustomUserAdmin)
