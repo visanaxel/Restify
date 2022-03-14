5699af432a5e8188e7d981305d65920732417810
chunkai <chunkai@chen.com>
2022-03-14 17:00:19 -0400

    updated serializers in notif

--- P2/notifications/serializers.py
+++ P2/notifications/serializers.py
@@ -1,25 +1,10 @@
 from rest_framework import serializers
-from notifications.models import RestNotifications
+from notifications.models import UserNotifications, OwnerNotifications
 from restaurant.models import Restaurant
 
 from users.models import MyUser
 
 rest_notification_choices = (('a', 'item added'), ('e', 'item edited'))
-
-class RestNotificationSerializer(serializers.ModelSerializer):
-    uid = MyUser
-    rid = Restaurant
-    notif_type = serializers.CharField(max_length=1)
-    description = serializers.CharField(max_length=100)
-
-    class Meta:
-        model = RestNotifications
-        fields = [
-            'uid',
-            'rid',
-            'notif_type',
-            'description'
-        ]
 
 class UserNotificationSerializer(serializers.ModelSerializer):
     uid = MyUser
@@ -28,7 +13,22 @@ class UserNotificationSerializer(serializers.ModelSerializer):
     description = serializers.CharField(max_length=100)
 
     class Meta:
-        model = RestNotifications
+        model = UserNotifications
+        fields = [
+            'uid',
+            'rid',
+            'notif_type',
+            'description'
+        ]
+
+class OwnerNotificationSerializer(serializers.ModelSerializer):
+    uid = MyUser
+    rid = Restaurant
+    notif_type = serializers.CharField(max_length=1)
+    description = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = OwnerNotifications
         fields = [
             'uid',
             'rid',
