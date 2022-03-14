cfcc7c0b26ca6835ada526f3e900716a09b25ef2
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-13 22:46:12 -0400

    Added serializers

--- P2/notifications/models.py
+++ P2/notifications/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.db.models import SET_NULL
 
@@ -5,12 +6,14 @@ from users.models import MyUser
 from blog.models import Blog
 from restaurant.models import Restaurant
 
+user_notification_choices = (('l', 'like'), ('f', 'follow'), ('c', 'comment'))
+rest_notification_choices = (('m', 'menu change'), ('n', 'new item'))
 # Create your models here.
 class UserNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
     bid = models.ForeignKey(to=Blog, on_delete=SET_NULL, null=True)
     # m = menu change, b = new blog posted
-    notif_type = models.CharField(max_length=1)
+    notif_type = models.CharField(max_length=1, choices=user_notification_choices)
     description = models.CharField(max_length=400)
     # create blog -> all followers follow this restaurant, save notification for each same with menu,
     # Notification.objects.save(uid = request.user, rid = restaurant)
@@ -19,4 +22,5 @@ class RestNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
     rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL, null=True)
     # l = like, f = follow, c = comment
-    notif_type = models.CharField(max_length=1)
+    notif_type = models.CharField(max_length=1, choices=rest_notification_choices)
+    description = models.CharField(max_length=100, default="")
---
+++ P2/notifications/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from notifications.models import RestNotifications
+from restaurant.models import Restaurant
+
+from users.models import MyUser
+
+rest_notification_choices = (('a', 'item added'), ('e', 'item edited'))
+
+class RestNotificationSerializer(serializers.ModelSerializer):
+    uid = MyUser
+    rid = Restaurant
+    notif_type = serializers.CharField(max_length=1)
+    description = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = RestNotifications
+        fields = [
+            'uid',
+            'rid',
+            'notif_type',
+            'description'
+        ]
