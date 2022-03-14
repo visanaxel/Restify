7e782af0cc30b6973df94e37ce1302646bb830a6
chunkai <chunkai@chen.com>
2022-03-13 23:57:50 -0400

    added addfollowserializer

--- P2/social/serializers.py
+++ P2/social/serializers.py
@@ -4,6 +4,9 @@ from restaurant.models import MenuItem;
 from restaurant.models import Restaurant
 from social.models import LikeRest
 
+from notifications.models import UserNotifications
+from social.models import Follows
+
 class LikeRestSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -13,3 +16,32 @@ class LikeRestSerializer(serializers.ModelSerializer):
     # validated data is what the user sent
     def create(self, validated_data):
         return validated_data.get('rid')
+
+class AddFollowSerializer(serializers.ModelSerializer):
+    pass
+
+    class Meta:
+        model = Follows
+        fields = ['rid']
+
+    def create(self, validated_data):
+        user =  self.context['request'].user
+        rid = validated_data.get('rid')
+        print(type(rid.id))
+        # Check if restaurant exists
+        restaurant = Restaurant.objects.filter(id = rid.id)
+        if (bool(restaurant) == False):
+            raise serializers.ValidationError({'error': 'restaurant does not exist'})
+
+        # check if person already follows
+        who_user_follows = Follows.objects.filter(uid = user, rid = restaurant[0])
+        print(who_user_follows)
+        if (bool(who_user_follows)):
+            raise serializers.ValidationError({'error': 'you already follow this person'})
+        
+        print(user)
+        notif = UserNotifications(rid=restaurant[0], uid = user, notif_type='f', description = user.username + " has followed you!")
+        notif.save()
+        follow = Follows(uid = user, rid = restaurant[0])
+        follow.save()
+        return follow
