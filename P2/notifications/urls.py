dae30d242ae94eb8ddb905a1e3943596db7cbacc
chunkai <chunkai@chen.com>
2022-03-14 17:00:34 -0400

    updated url in notif

--- P2/notifications/urls.py
+++ P2/notifications/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from notifications.views import UserNotificationView, RestNotificationView
+from notifications.views import UserNotificationView, OwnerNotificationView
 
 app_name = 'notifications'  
   
 urlpatterns = [
     path('user/', UserNotificationView.as_view(), name='user_notif' ),
-    path('restaurant/',  RestNotificationView.as_view(), name='rest_notif'),
+    path('owner/',  OwnerNotificationView.as_view(), name='owner_notif'),
 ]
