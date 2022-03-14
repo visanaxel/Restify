d9a93be56d42485dce53587cb7d31e4a9d256377
chunkai <chunkai@chen.com>
2022-03-14 02:27:47 -0400

    urls for notifications w/Danny

--- P2/notifications/urls.py
+++ P2/notifications/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 
+from notifications.views import UserNotificationView, RestNotificationView
+
 app_name = 'notifications'  
   
 urlpatterns = [
-    
+    path('user/', UserNotificationView.as_view(), name='user_notif' ),
+    path('restaurant/',  RestNotificationView.as_view(), name='rest_notif'),
 ]
