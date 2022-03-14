f1678df6a669fc5200eaddde0ebeb66c71fbf92e
chunkai <chunkai@chen.com>
2022-03-14 00:03:24 -0400

    changed attribute in user notif

--- P2/notifications/models.py
+++ P2/notifications/models.py
@@ -8,19 +8,18 @@ from restaurant.models import Restaurant
 
 user_notification_choices = (('l', 'like'), ('f', 'follow'), ('c', 'comment'))
 rest_notification_choices = (('m', 'menu change'), ('n', 'new item'), ('b', 'blog added'))
+
 # Create your models here.
 class UserNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
-    bid = models.ForeignKey(to=Blog, on_delete=SET_NULL, null=True)
-    # m = menu change, b = new blog posted
+    rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL, null=True)
+    # m(('l', 'like'), ('f', 'follow'), ('c', 'comment'))
     notif_type = models.CharField(max_length=1, choices=user_notification_choices)
     description = models.CharField(max_length=400)
-    # create blog -> all followers follow this restaurant, save notification for each same with menu,
-    # Notification.objects.save(uid = request.user, rid = restaurant)
 
 class RestNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
     rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL, null=True)
-    # l = like, f = follow, c = comment
+    # (('m', 'menu change'), ('n', 'new item'), ('b', 'blog added'))
     notif_type = models.CharField(max_length=1, choices=rest_notification_choices)
     description = models.CharField(max_length=100, default="")
