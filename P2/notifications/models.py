82033879207fc0c15896d0e63076871ba67565cd
chunkai <chunkai@chen.com>
2022-03-14 16:59:43 -0400

    fixed model name

--- P2/notifications/models.py
+++ P2/notifications/models.py
@@ -6,22 +6,19 @@ from users.models import MyUser
 from blog.models import Blog
 from restaurant.models import Restaurant
 
-user_notification_choices = (('l', 'like'), ('f', 'follow'), ('c', 'comment'))
-rest_notification_choices = (('m', 'menu change'), ('n', 'new item'), ('b', 'blog added'))
+owner_notification_choices = (('l', 'like'), ('f', 'follow'), ('c', 'comment'))
+user_notification_choices = (('m', 'menu change'), ('n', 'new item'), ('b', 'blog added'))
 
-# Create your models here.
-# THIS IS BEING SENT TO THE RESTAURANT!!!!!
-class UserNotifications(models.Model):
+class OwnerNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
     rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL, null=True)
     # m(('l', 'like'), ('f', 'follow'), ('c', 'comment'))
-    notif_type = models.CharField(max_length=1, choices=user_notification_choices)
+    notif_type = models.CharField(max_length=1, choices=owner_notification_choices)
     description = models.CharField(max_length=400)
 
-# THIS IS BEING SENT TO THE USER!!!!!!
-class RestNotifications(models.Model):
+class UserNotifications(models.Model):
     uid = models.ForeignKey(to=MyUser, on_delete=SET_NULL, null=True)
     rid = models.ForeignKey(to=Restaurant, on_delete=SET_NULL, null=True)
     # (('m', 'menu change'), ('n', 'new item'), ('b', 'blog added'))
-    notif_type = models.CharField(max_length=1, choices=rest_notification_choices)
+    notif_type = models.CharField(max_length=1, choices=user_notification_choices)
     description = models.CharField(max_length=100, default="")
