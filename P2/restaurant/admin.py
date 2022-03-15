5da2cfc2f71199f0dbc3ad3d38f7256afdf89533
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-03-14 21:30:44 -0400

    added view comments

--- P2/restaurant/admin.py
+++ P2/restaurant/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from restaurant.models import Restaurant, MenuItem
+from restaurant.models import Comment, Restaurant, MenuItem
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(MenuItem)
+admin.site.register(Comment)
