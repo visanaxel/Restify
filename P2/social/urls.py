a9010ec8c2cf60b6413919ff502f27d13147a3dd
chunkai <chunkai@chen.com>
2022-03-14 21:33:02 -0400

    added feed url

--- P2/social/urls.py
+++ P2/social/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from social.views import AddFollowView, DeleteFollowApiView
+from social.views import AddFollowView, DeleteFollowApiView, FeedView
 from social.views import LikeRestView, UnlikeRestaurant, LikeBlogView, UnlikeBlogView
 
 app_name = 'social'  
@@ -11,5 +11,6 @@ urlpatterns = [
     path('like/restaurant/', LikeRestView.as_view(), name='like_rest'),
     path('unlike/restaurant/<rid>', UnlikeRestaurant.as_view(), name='unlike_rest'),
     path('like/blog/', LikeBlogView.as_view(), name='like_blog'),
-    path('unlike/blog/<bid>', UnlikeBlogView.as_view(), name='unlike_blog')
+    path('unlike/blog/<bid>', UnlikeBlogView.as_view(), name='unlike_blog'),
+    path('feed/', FeedView.as_view(), name='feed')
     ]
