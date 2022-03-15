d02c219a311e0ecbff8a721e65dee5f5ce86808c
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-03-15 19:08:30 -0400

    added phone numbers

--- P2/users/models.py
+++ P2/users/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models import SET_NULL
@@ -7,6 +8,8 @@ class MyUser(AbstractUser):
     profile_pic = models.ImageField(default='https://cbwc.ca/wp-content/uploads/2019/03/blank-profile-picture-973460_640.png') # Change to local pic
     # restauarant user or regular user
     is_owner = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20) 
+
 
 
 
