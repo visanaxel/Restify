1247d32cc1ee2f3c8acf520339b78006b53c2fbc
visanaxel <axel.visan@mail.utoronto.ca>
2022-03-15 21:11:31 -0400

    Mesage changed

--- P2/users/serializers.py
+++ P2/users/serializers.py
@@ -3,11 +3,9 @@ from rest_framework import serializers;
 from users.models import MyUser
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    # youtube dude didnt have to import but he didnt extend so I dunno tbh nvm he did
 
     password = serializers.CharField(required=True, write_only = True)
     password2 = serializers.CharField(required=True, write_only = True)
-    # username = serializers.CharField(required=True)
     
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
@@ -29,8 +27,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             'password2': {'write_only': True}
         }
     
-    # Every serelizer over rides this function I guess to save changes to the model i dunno
-    # validated data is what the user sent
     def create(self, validated_data):
         print("entered create method")
         username = validated_data.get('username')
@@ -49,13 +45,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             user = MyUser(username=username, email=email, profile_pic=profile_pic, first_name = first_name, last_name=last_name, phone_number=phone_number)
             user.set_password(password)
             user.save()
-            # why return user I dont know youtube dude it
             return user
         else:
             raise serializers.ValidationError({'error': 'Passwords don\'t match'})
 
-        # might need to uncomment this
-        # return super().create(validated_data)
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -69,16 +62,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             'last_name',
             'email',
             'phone_number'
-            # 'profile_pic'
         ]
 
-
-    # def update(self, instance, validated_data):
-    #     print("enter uodae")
-    #     instance.username = validated_data.get('username', instance.username)
-    #     instance.email = validated_data.get('email', instance.email)
-    #     instance.save()
-    #     return instance
 
 class MyUserSerializer(serializers.ModelSerializer):
 
