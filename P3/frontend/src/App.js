5658d1c4664f7fe1d0f163a15b4b97cdc13f8034
dxnny8 <danyal.ilyas@mail.utoronto.ca>
2022-04-15 16:38:25 -0400

    resolving

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Register } from './pages/Register';
+import { Register } from './pages/Register/Register';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
