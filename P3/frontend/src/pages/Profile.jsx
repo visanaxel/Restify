940f332547c43089b4330c1fb148e20b8259eb8a
visanaxel <axel.visan@mail.utoronto.ca>
2022-04-15 18:33:41 -0400

    bare

--- P3/frontend/src/pages/Profile.jsx
+++ P3/frontend/src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import Button from '../components/Button'
 import Navbar from "../components/Navbar/navbar";
 import Footer from "../components/Footer/footer";
 import Card from "../components/Card/card"
+import axios from 'axios';
+import Axios from 'axios';
 
 
 class Profile extends React.Component {
@@ -27,30 +29,38 @@ class Profile extends React.Component {
 export const ProfileView = () => {
     return new ProfileView2();
 }
+// const makePost = () => {
+//     console.log("here")
+//     let response
+
+//     response = axios.get(`http://127.0.0.1:8000/users/profile`) {
+//         headers: {
+//             "Authorization": `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUwMTQyNzkyLCJpYXQiOjE2NTAwNTYzOTIsImp0aSI6IjE5ZDY1MDU0MDg1MzRhYTRhYTk5MjhjOTFjNDgyOGVkIiwidXNlcl9pZCI6MX0.gC93XS-4HbZeaRgszLc8x4lJyc27S4RzxgOFuyB7AtM`
+//         }
+//     }
+
+//     let data = response.data
+//     console.log("data" + data)
+// }
+const getUser = () => {
+
+    console.log("CALLED")
+
+    const x = []
+    let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUwMTQ0NTgxLCJpYXQiOjE2NTAwNTgxODEsImp0aSI6ImNmMzVhOTZlN2IxODQ2YzFiOWI2N2IzMWM2ZjJhYjljIiwidXNlcl9pZCI6MX0.7z2TatDXOjZSEOL3BYADnq5e9wLUB-4OqWqXHuUcjD8"
+    const config = {
+        headers: { Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUwMTQ0NTgxLCJpYXQiOjE2NTAwNTgxODEsImp0aSI6ImNmMzVhOTZlN2IxODQ2YzFiOWI2N2IzMWM2ZjJhYjljIiwidXNlcl9pZCI6MX0.7z2TatDXOjZSEOL3BYADnq5e9wLUB-4OqWqXHuUcjD8"}    
+    };
+    const data = {};
+
+    Axios.get('http://127.0.0.1:8000/users/profile/', data, config)
+        .then(result => result.data)
+        .then(data2 => {
+            console.log(data2)
+        })
+}
 
 export default class ProfileView2 extends React.Component {
-    async retrieveInfo () {
-        let response
-        if(this.props.private !== true){
-            const username = this.props.kwargs.username
-            response = await axios.get(`http://localhost:5000/api/Profiles/${username}`,{ 
-                    headers: {
-                        "Authorization": `Bearer ${localStorage.getItem('token')}`
-                    }
-                }
-            )
-        }
-        else{
-            response = await axios.get(`http://localhost:5000/api/Profiles/`,{ 
-                    headers: {
-                        "Authorization": `Bearer ${localStorage.getItem('token')}`
-                    }
-                }
-            )
-        }
-        let data = response.data
-        
-    }
 
     render() {
         return (
@@ -58,8 +68,9 @@ export default class ProfileView2 extends React.Component {
                 <link rel="stylesheet" href="profile.css" />
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
                 <Navbar />
+                {getUser()}
                 <p className="blog">Profile</p>
-                <Card/>
+                <Card />
                 <Footer />
             </div>
         )
