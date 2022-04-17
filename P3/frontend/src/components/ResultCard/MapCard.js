d98e5f18dd04cd2643b158ca62a56ef37db7f5d1
chunkai <chunkai@chen.com>
2022-04-17 09:30:39 -0400

    make mapcard

---
+++ P3/frontend/src/components/ResultCard/MapCard.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../../pages/Home/Home.css';
+
+function MapCard(props) {
+
+    if (props['data']['results'] !== undefined) {
+
+        var result = props['data']['results'].reduce((resultArray, item, index) => { 
+            const chunkIndex = Math.floor(index/3)
+          
+            if(!resultArray[chunkIndex]) {
+              resultArray[chunkIndex] = [] // start a new chunk
+            }
+          
+            resultArray[chunkIndex].push(item)
+          
+            return resultArray
+        }, [])
+
+        return (
+            <>
+            <div id="top_res_cards">
+
+                { result.map( (sublist, i) => { 
+                    console.log(sublist);
+                    return (
+                        <> 
+                            <div className="card-deck card_row"> { sublist.map( (element, j) => {
+                                return (<>
+                                    <div className="card">
+                                        <a href="../restaurant_page/restaurant.html">
+                                            <img className="card-img-top" src={element['logo']} alt="Card image cap"/>
+                                            <div className="card-body">
+                                                <h5 className="card-title">{element['name']}</h5>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </>)
+
+                            })  }</div> 
+                        </>
+                    )
+                })}
+            </div>
+            </>
+        )
+
+    }
+
+}
+
+export default MapCard;
+
+{/* <div id="top_res_cards">
+    <div className="card-deck card_row">
+        <div className="card">
+            <a href="../restaurant_page/restaurant.html">
+                <img className="card-img-top" src={props['data']['results'][0]['logo']} alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">{props['data']['results'][0]['name']}</h5>
+                </div>
+            </a>
+        </div>
+    </div>
+</div> */}
