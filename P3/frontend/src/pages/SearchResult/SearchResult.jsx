42af77c27b2ed2b15a5e6667fa2f3f85c4fe40e2
chunkai <chunkai@chen.com>
2022-04-17 09:31:36 -0400

    added search result

--- P3/frontend/src/pages/SearchResult/SearchResult.jsx
+++ P3/frontend/src/pages/SearchResult/SearchResult.jsx
@@ -4,41 +4,64 @@ import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/footer";
 import Search from "../../components/Search/Search";
 import SearchBar from "../../components/Search/SearchBar";
-import { useState, useEffect, useParams } from 'react';
+
+import ResultCard from '../../components/ResultCard/ResultCard';
+
+import { useState, useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+
 import axios from 'axios';
 import Axios from 'axios';
+import MapCard from '../../components/ResultCard/MapCard';
+
+
 export const SearchResult = () => {
+
+    const [data, setData] = useState([])
 
     var query = useParams()['query'];
 
-    const [items, setItems] = useState([]);
-
+    var url = 'http://127.0.0.1:8000/restaurant/search/?query=' + query;
+    console.log(url)
 
     useEffect(() => {
-        axios.post("http://127.0.0.1:8000/restaurant/search/?query=" + query).then(result => result.data)
-            .then(data2 => {
-                console.log(data2)
-                console.log(data2['results'])
-                setItems(data2['results']);
-                console.log(data2['results'].length)
-            })
-    }, []);
+        fetch(url, {method: 'GET'})
+        .then(response => {
+            //console.log('Hi');
+            return response.json();
+        })
+        .then(json => {
+            //console.log(json);
+            setData(json);
+        });
+    }, [])
 
-    return (
-        <>
-            <div>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                crossOrigin="anonymous"></script>
-            </div>
+    if (data !== []) {
+        console.log(data)
+        return (
+            <>
+                <div>
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                        crossOrigin="anonymous"></script>
+                    <link rel='stylesheet' href='./Search.css'/>
+                </div>
 
-            <Navbar></Navbar>
+                <Navbar></Navbar>
 
-            <SearchBar></SearchBar>
+                <SearchBar></SearchBar>
 
-            <Footer></Footer>
-        </>
-    );
+                <div class="card-body">
+                    <h4>Results</h4>
+                </div>
+
+                <MapCard data={data}></MapCard>
+
+                <Footer></Footer>
+            </>
+        );
+    }
 }
