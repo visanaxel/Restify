ba63a57efe0f383c96416a287aefe4b7645e23ef
chunkai <chunkai@chen.com>
2022-04-17 09:48:29 -0400

    updated navbar

--- P3/frontend/src/components/Navbar/navbar.js
+++ P3/frontend/src/components/Navbar/navbar.js
@@ -4,17 +4,14 @@ function Navbar() {
   return ( <>
 <header id="nav">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <a className="navbar-brand" href="../home_page/home.html">🆁🅴🆂🆃🅸🅵🆈</a>
+              <a className="navbar-brand" href="http://localhost:3000/home">🆁🅴🆂🆃🅸🅵🆈</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item active">
-                    <a className="nav-link" href="../home_page/home.html">Home <span className="sr-only">(current)</span></a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="../blog_post_page/blog.html">Feed</a>
+                    <a className="nav-link" href="http://localhost:3000/home">Home <span className="sr-only">(current)</span></a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="../restaurant_page/restaurant.html">My Restaurant</a>
