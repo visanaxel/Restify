077cdd379aba2cbe9e6126779a981463fe930137
chunkai <chunkai@chen.com>
2022-04-17 22:23:22 -0400

    user notification good

--- P3/frontend/src/components/Footer/footer.js
+++ P3/frontend/src/components/Footer/footer.js
@@ -6,7 +6,7 @@ function Footer() {
   bottom: '0',
   width: '100%',
   height: '100' }} className="text-center text-lg-start bg-light text-muted">
-            <div className="text-center p-4" style={{backgroundColor: 'rgba(0, 0, 0, 0.05)'}}>
+            <div className="text-center " style={{backgroundColor: 'rgba(0, 0, 0, 0.05)', paddingTop: '20', paddingBottom: '20'}}>
               © 2022 Copyright:
               <a className="text-reset fw-bold" href="https://mdbootstrap.com/">Restify.com</a>
             </div>
