b9c48754870eab6e18eecfd6b65c5f24c507e72c
chunkai <chunkai@chen.com>
2022-04-18 16:18:43 -0400

    Feed good

--- P3/frontend/src/components/NotifCard/FeedCard.js
+++ P3/frontend/src/components/NotifCard/FeedCard.js
@@ -8,7 +8,7 @@ function FeedCard(props) {
      let nav = useNavigate()
 
     function go(bid, rid) {
-        nav('blog/' + bid + '/' + rid);
+        nav('../blog/' + bid + '/' + rid);
     }
 
     if (props['data']['results'] !== undefined) {
@@ -27,7 +27,7 @@ function FeedCard(props) {
                         <div className="col-2 picture_column d-flex justify-content-end">
                             <img className="profile_pic" src={item['image']}/>
                         </div>
-                        <div className="col-8" onClick={go(item['id'], item['rid'])}>
+                        <div className="col-8" onClick={() => go(item['id'], item['rid'])}>
                             <div className="alert alert-primary alert-dismissible fade show" role="alert">
 
                                 <h4 className="alert-heading">{item['title']}</h4>
