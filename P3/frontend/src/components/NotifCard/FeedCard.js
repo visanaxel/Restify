5c19f5e51dc38d28d19f74f5a85e51618445b0e4
chunkai <chunkai@chen.com>
2022-04-18 16:13:08 -0400

    new

---
+++ P3/frontend/src/components/NotifCard/FeedCard.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import '../../pages/Notification/notification.css';
+import { useNavigate } from 'react-router-dom';
+
+function FeedCard(props) {
+
+     let nav = useNavigate()
+
+    function go(bid, rid) {
+        nav('blog/' + bid + '/' + rid);
+    }
+
+    if (props['data']['results'] !== undefined) {
+
+        var result = props['data']['results'];
+
+        //console.log(result);
+
+        return (
+            <>
+            {result.map((item, i) => {
+
+                return (
+                    <>
+                    <div className="row mt-0" >
+                        <div className="col-2 picture_column d-flex justify-content-end">
+                            <img className="profile_pic" src={item['image']}/>
+                        </div>
+                        <div className="col-8" onClick={go(item['id'], item['rid'])}>
+                            <div className="alert alert-primary alert-dismissible fade show" role="alert">
+
+                                <h4 className="alert-heading">{item['title']}</h4>
+                                <p>{item['content'].substring(0, 200) + '...'}</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    </>
+                )
+            })}
+            </>
+        );
+    }
+}
+
+export default FeedCard;
