0feafa04ecda087a5b024fc73a0c71d40d6a1e54
chunkai <chunkai@chen.com>
2022-04-17 20:51:48 -0400

    modified setttings in backend and frontend

--- P3/backend/p2_project/settings.py
+++ P3/backend/p2_project/settings.py
@@ -141,7 +141,7 @@ AUTH_USER_MODEL = 'users.MyUser'
 REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 6,
     
     'DEFAULT_AUTHENTICATION_CLASSES': (
         
